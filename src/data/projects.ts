231dcc24f12117a188c9173b5e10149b7ccea43d
haard18 <haardsolanki.itm@gmail.com>
2024-08-21 20:23:28 +0530

    added the images for project cards

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,30 +1,34 @@
+import dumdum from '../data/images/dumdum.png'
+import sam from '../data/images/sam.png'
+import churn from '../data/images/churn.png'
+import opus from '../data/images/opus.png'
 export const projects = [
     {
         "name": "DumDumDeploy",
         "description": "A simple deployment tool for deploying your projects on Arweave",
         "url": "https://dumdumdeploy.vercel.app",
         "github": "https://github.com/ankushkun/dumdumdeploy",
-        "image": "src/data/images/dumdum.png"
+        "image": dumdum
     },
     {
         "name": "SAM-SUPPORT",
         "description": "End to End Support Mechanism for Arweave and AO processes",
         "url": "https://sam-support.arweave.net",
         "github": "https://github.com/krishvsoni/sam",
-        "image": "src/data/images/sam.png"
+        "image":sam
     },
     {
         "name": "Churn-O-Analyzer",
         "description": "Customer Churn Analyzer for Telecom Industry",
         "url": "https://churn-frontend-rho.vercel.app/",
         "github": "https://github.com/haard18/churn-frontend",
-        "image": "src/data/images/churn.png"
+        "image": churn
     },
     {
         "name": "Opus Stellar",
         "description": "Buy your own piece of LAND in your favourite Metaverse",
         "url": "https://devfolio.co/projects/opus-stellar-70b7",
         "github": "https://github.com/OctoTechHub/Opus",
-        "image": "src/data/images/opus.png"
+        "image": opus
     }
 ]
