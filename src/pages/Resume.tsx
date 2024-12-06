c6858d93cf5b735e86d8eec8c89efd51425bdbbf
Haard Solanki <128149378+haard18@users.noreply.github.com>
2024-12-06 14:45:32 +0530

    Update Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PDFViewer from "../components/Pdfviewer";
-import resume from "../data/resume_updated.pdf"
+import resume from "../data/resume_template.pdf"
 import BackButton from "../components/backButton";
 const Resume: React.FC = () => {
   return (
