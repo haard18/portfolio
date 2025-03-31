72eb567f30ce918e110b10e003b940e8c555d6cd
haard18 <haardsolanki.itm@gmail.com>
2025-03-31 23:52:07 +0530

    Replace resume_template.pdf with new_resume.pdf in Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PDFViewer from "../components/Pdfviewer";
-import resume from "../resume_template.pdf";
+import resume from "../data/new_resume.pdf";
 import BackButton from "../components/backButton";
 import { AwesomeButton } from "react-awesome-button";
 
