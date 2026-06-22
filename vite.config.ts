162766edaebc733bddc8773424975fc14c6a3881
haard18 <haardsolanki.itm@gmail.com>
2026-06-22 15:02:34 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
- 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+
+export default defineConfig({})
